52e0b27b712726441ada28054624e82da059910a
cicciog <cicciog>
2020-04-07 19:04:44 +0200

    initial commit

---
+++ src/main/java/DockerController.java
@@ -0,0 +1,24 @@
+/**
+ *
+ * @author cicciog
+ */
+public class DockerController {
+
+    public DockerController() {
+    }
+    
+    public void checkDockerVersion(){
+        
+    }
+    
+    public void getAllDockerContainers(){
+        
+    }
+    
+    public void getAllDockerImages(){
+        
+    }
+    
+    
+    
+}
