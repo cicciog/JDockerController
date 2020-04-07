a05524f32fae1c29f84abae65241281390ce46b3
cicciog <cicciog>
2020-04-07 19:35:02 +0200

    create methods checkDockerVersion and getAllDockerContainers

--- src/main/java/DockerController.java
+++ src/main/java/DockerController.java
@@ -1,17 +1,49 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  *
  * @author cicciog
  */
 public class DockerController {
+    private ProcessBuilder processBuilder;
 
     public DockerController() {
+        this.processBuilder = new ProcessBuilder();
     }
     
-    public void checkDockerVersion(){
-        
+    public void checkDockerVersion() throws IOException, InterruptedException{
+         processBuilder.command("powershell.exe", "/c", "docker version > C:\\Users\\Franecesco-pc\\Documents\\NetBeansProjects\\JDockerController\\JDockerController\\output\\version.txt");
+         Process process = processBuilder.start();
+
+            BufferedReader reader =
+                    new BufferedReader(new InputStreamReader(process.getInputStream()));
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+
+            int exitCode = process.waitFor();
+            System.out.println("\nExited with error code : " + exitCode);
     }
     
-    public void getAllDockerContainers(){
+    public void getAllDockerContainers() throws IOException, InterruptedException{
+                 processBuilder.command("powershell.exe", "/c", "docker ps > C:\\Users\\Franecesco-pc\\Documents\\NetBeansProjects\\JDockerController\\JDockerController\\output\\containers.txt");
+         Process process = processBuilder.start();
+
+            BufferedReader reader =
+                    new BufferedReader(new InputStreamReader(process.getInputStream()));
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+
+            int exitCode = process.waitFor();
+            System.out.println("\nExited with error code : " + exitCode);
         
     }
     
---
+++ src/main/java/Main.java
@@ -0,0 +1,27 @@
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Franecesco-pc
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        DockerController dockerController = new DockerController();
+        try {
+            dockerController.checkDockerVersion();
+            dockerController.getAllDockerContainers();
+        } catch (IOException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (InterruptedException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+}
