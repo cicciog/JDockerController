00071300135079df86600f419265159a8c648124
cicciog <f.gualtieri@live.it>
2020-04-24 18:48:49 +0200

    removed unecessary CSV files

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,4 @@
 
-import com.opencsv.CSVReader;
-import com.opencsv.bean.CsvToBean;
-import com.opencsv.bean.HeaderColumnNameTranslateMappingStrategy;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 /**
  *
  * @author cicciog
