b7ca09410d4494e140f50f21d98fd64169cde6df
cicciog <f.gualtieri@live.it>
2020-04-29 10:49:00 +0200

    unit test of method checkDockerVarsion

--- src/main/java/dockerController/DockerController.java
+++ src/main/java/dockerController/DockerController.java
@@ -5,9 +5,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Set;
+
 
 /**
  *
--- src/test/java/dockerController/TestDockerController.java
+++ src/test/java/dockerController/TestDockerController.java
@@ -1,8 +1,12 @@
 package dockerController;
 
+import java.io.IOException;
 import java.sql.Time;
+import java.util.HashMap;
+import static junit.framework.TestCase.assertFalse;
 import static junit.framework.TestCase.assertNotNull;
 import static junit.framework.TestCase.assertTrue;
+import org.apache.commons.collections.map.HashedMap;
 import org.junit.Test;
 
 /**
@@ -14,7 +18,7 @@ public class TestDockerController {
     DockerController dockerController = new DockerController();
     
     @Test
-    public void testAdd() throws InterruptedException {
+    public void testAdd() throws InterruptedException, IOException {
         
         //check if the object is not null
         assertNotNull(dockerController);
@@ -27,6 +31,15 @@ public class TestDockerController {
         assertTrue(end < (int) (System.currentTimeMillis()/1000));
         assertTrue(end > start);
         
+        //check if the hashmap is not empty
+        HashMap hm = new HashMap();
+        hm = dockerController.checkDockerVersion();
+        assertTrue(hm.size() > 0);
+        assertFalse(hm.isEmpty());
+        
+        //check if exist this keys
+        assertTrue(hm.containsKey("client"));
+        assertTrue(hm.containsKey("server"));
     }
     
 }
