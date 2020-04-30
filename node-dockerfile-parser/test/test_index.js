689edb48098c8322f0eba34a67bcbed939031878
cicciog <f.gualtieri@live.it>
2020-04-30 16:18:00 +0200

    create unit test for createJson in mocha :smile: :bug: :bug:

--- node-dockerfile-parser/test/test_index.js
+++ node-dockerfile-parser/test/test_index.js
@@ -3,6 +3,8 @@ const mocha = require('mocha');
 const assert = require('assert');
 const dockerFileManager = require('../dockerFileManager');
 const repositories = "/home/francesco/Documenti/repositories";
+const options = {includeComments: false};
+const parser = require('docker-file-parser');
 
 
 describe("Unit Test 1", function () {
@@ -23,3 +25,14 @@ describe("Unit Test 2", function () {
     });
 });
 
+describe("Unit Test 3", function () {
+    
+    it("should be able to verify if a generated json file exist", function () {
+        var images = dockerFileManager.csvToArray('../DokerBuildImagesCmd.csv');
+        var dockerfile = dockerFileManager.readDockerfile(repositories + images[1].buildcommand.split('\\').join('/'));
+        var commands = parser.parse(dockerfile,options);
+        dockerFileManager.createJson(images[1].name,commands);
+        assert.equal(fs.existsSync('../../output/'+images[1].name+'.json'),true,'the file '+images[1].name+' exist');
+        fs.unlinkSync('../../output/'+images[1].name+'.json');
+    });
+});
