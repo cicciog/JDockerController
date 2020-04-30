b34a6bf09e1fe550fbafab067a5c834be386f23e
cicciog <f.gualtieri@live.it>
2020-04-30 13:08:09 +0200

    create two unit test with mocha of dockerFileManager.js

--- node-dockerfile-parser/test/test_index.js
+++ node-dockerfile-parser/test/test_index.js
@@ -1,14 +1,25 @@
 const fs = require('fs');
-const tape = require('tape');
+const mocha = require('mocha');
+const assert = require('assert');
 const dockerFileManager = require('../dockerFileManager');
+const repositories = "/home/francesco/Documenti/repositories";
 
 
-tape('every cmd', function (t) {
-    t.plan(1);
+describe("Unit Test 1", function () {
     
-    dockerImage = dockerFileManager.csvToArray('DokerBuildImagesCmd.csv'); 
-   
-    t.equal('ciao','ciao');
+    it("should be able to verify the array length", function () {
+        var images = dockerFileManager.csvToArray('../DokerBuildImagesCmd.csv');
+        
+        assert.equal(images.length > 0,true,'array contains a number o element greater than 0');
+    });
 });
 
+describe("Unit Test 2", function () {
+    
+    it("should be able to verify the array length", function () {
+        var images = dockerFileManager.csvToArray('../DokerBuildImagesCmd.csv');
+        dockerfile = dockerFileManager.readDockerfile(repositories + images[1].buildcommand.split('\\').join('/'));
+        assert.equal(dockerfile.length > 0,true,'dockerfile contains all lines of the file specified');
+    });
+});
 
