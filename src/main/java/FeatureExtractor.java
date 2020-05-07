221c0c618f07293da3fb2aebf5bac08588074c6b
cicciog <f.gualtieri@live.it>
2020-05-07 11:04:07 +0200

    create method extractAllImageFeatures in FeatureExtractor Class :octopus: :ocean: :whale:

--- src/main/java/FeatureExtractor.java
+++ src/main/java/FeatureExtractor.java
@@ -1,8 +1,66 @@
 
+import fileManager.FileManager;
+import fileManager.Path;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
 /**
  *
- * @author francesco
+ * @author cicciog
  */
 public class FeatureExtractor {
-    
+
+    private FileManager fileManager = new FileManager();
+    private Path path = new Path();
+    private String features[] = {"FROM",
+        "LABEL",
+        "ENV",
+        "RUN",
+        "VOLUME",
+        "COPY",
+        "ENTRYPOINT",
+        "CMD",
+        "WORKDIR",
+        "USER",
+        "EXPOSE",
+        "MAINTAINER",
+        "ARG",
+        "STOPSIGNAL",
+        "ADD",
+        "SHELL"};
+    private List<String> listOfFeatures = Arrays.asList(features);
+    private FeaturesEntity featuresEntity;
+
+    public FeaturesEntity extractAllImageFeatures(String pSource) throws FileNotFoundException, IOException, ParseException {
+
+        JSONParser parser = new JSONParser();
+        featuresEntity = new FeaturesEntity(pSource);
+
+        Reader reader = new FileReader(fileManager.getWorkDirectory() + path.getInput() + "/json/" + pSource);
+
+        JSONObject jsonObject = (JSONObject) parser.parse(reader);
+        //System.out.println(jsonObject);
+
+        // loop array
+        JSONArray docker = (JSONArray) jsonObject.get("docker");
+        for (int i = 0; i < docker.size(); i++) {
+            JSONObject image = (JSONObject) docker.get(i);
+
+            if (listOfFeatures.contains(image.get("name"))) {
+                featuresEntity.addFeature(image.get("name").toString().trim());
+            }
+        }
+
+        return featuresEntity;
+    }
+
 }
