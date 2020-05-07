ed27d9070007b44586ff57a39d824b6887942238
cicciog <f.gualtieri@live.it>
2020-05-07 18:29:26 +0200

    create unit test for method readDockerImagesFeaturesFromFile :boom: :boom: :boom:

--- src/test/java/fileManager/TestCSVmanager.java
+++ src/test/java/fileManager/TestCSVmanager.java
@@ -19,7 +19,7 @@ import org.junit.Test;
  * @author cicciog
  */
 public class TestCSVmanager {
-    
+
     CSVmanager csvmanager = new CSVmanager();
     FileManager filemanager = new FileManager();
     Path path = new Path();
@@ -27,7 +27,7 @@ public class TestCSVmanager {
     ArrayList<String[]> images1;
     ArrayList<Repository> repositories;
     ArrayList<DockerImage> dockerImages;
-    
+
     @Test
     public void testAdd() throws URISyntaxException, IOException {
 
@@ -62,12 +62,12 @@ public class TestCSVmanager {
         assertTrue(dockerImages.get(0).getName().length() > 0);
         assertTrue(dockerImages.get(0).getCommand().length() > 0);
         assertTrue(dockerImages.get(0).getNumberOfBuild() == 10);
-        
+
         float sum = 0;
         for (int i = 0; i < 10; i++) {
             sum = sum + dockerImages.get(0).getBuildingTime()[i];
         }
-        
+
         assertEquals(dockerImages.get(0).getAverageBuildTime(), sum / 10);
 
         //check if the file will be write
@@ -75,24 +75,24 @@ public class TestCSVmanager {
         csvmanager.writeDockerImagesMultipleBuildResult(dockerImages, "DockerBuildImages_1.csv");
         assertTrue(filemanager.fileExist(filemanager.getWorkDirectory() + path.getOutput() + "/DockerBuildImages_1.csv"));
         filemanager.deleteFile(filemanager.getWorkDirectory() + path.getOutput() + "/DockerBuildImages_1.csv");
-        
+
         DockerImage dockerImage = new DockerImage("example", "sudo docker build -t example .");
         dockerImage.setBuildable(true);
         dockerImage.addOneTimeDockerImageBuild(100);
-        
+
         assertFalse(filemanager.fileExist(filemanager.getWorkDirectory() + path.getOutput() + "/DokerBuildImagesDataSet[prova].csv"));
         csvmanager.writeDockerImageSingleBuildResult(dockerImage, "DokerBuildImagesDataSet[prova].csv");
         assertTrue(filemanager.fileExist(filemanager.getWorkDirectory() + path.getOutput() + "/DokerBuildImagesDataSet[prova].csv"));
-        
-        filemanager.deleteFile(filemanager.getWorkDirectory() + path.getOutput() + "/DokerImagesFeatures[prova].csv");
-        
+
+        filemanager.deleteFile(filemanager.getWorkDirectory() + path.getOutput() + "/DokerBuildImagesDataSet[prova].csv");
+
         FeaturesEntity featureEntity = new FeaturesEntity("heroguard");
         featureEntity.setFROM(1);
         featureEntity.setRUN(6);
         featureEntity.setMAINTAINER(1);
         featureEntity.setCMD(1);
         featureEntity.setNumberOfFeatures(9);
-        
+
         FeaturesEntity featureEntity1 = new FeaturesEntity("ImageAnalyzer");
         featureEntity1.setFROM(1);
         featureEntity1.setRUN(8);
@@ -101,23 +101,36 @@ public class TestCSVmanager {
         featureEntity1.setVOLUME(1);
         featureEntity1.setCMD(1);
         featureEntity1.setNumberOfFeatures(15);
-        
+
         assertNotNull(featureEntity);
         assertNotNull(featureEntity1);
-        
+
         ArrayList<FeaturesEntity> list = new ArrayList<>();
         list.add(featureEntity);
         list.add(featureEntity1);
-        
+
         assertTrue(list.size() > 0);
-        assertEquals(list.size(),2);
-        assertSame(list.size(),2);
-        
+        assertEquals(list.size(), 2);
+        assertSame(list.size(), 2);
+
         assertFalse(filemanager.fileExist(filemanager.getWorkDirectory() + path.getOutput() + "/DokerImagesFeatures[prova].csv"));
-        csvmanager.writeDockerImagesFeaturesFromList(list,filemanager.getWorkDirectory() + path.getOutput() + "/DokerImagesFeatures[prova].csv");
+        csvmanager.writeDockerImagesFeaturesFromList(list, filemanager.getWorkDirectory() + path.getOutput() + "/DokerImagesFeatures[prova].csv");
         assertTrue(filemanager.fileExist(filemanager.getWorkDirectory() + path.getOutput() + "/DokerImagesFeatures[prova].csv"));
-        
-        filemanager.deleteFile(filemanager.getWorkDirectory() + path.getOutput() + "/DokerImagesFeatures[prova]");
-        
+
+        ArrayList<FeaturesEntity> features = (ArrayList<FeaturesEntity>) csvmanager.readDockerImagesFeaturesFromFile("DokerImagesFeatures[prova].csv");
+        assertTrue(features.size() > 0);
+        assertEquals(features.size(), 2);
+        assertSame(features.size(), 2);
+
+        assertEquals(featureEntity.toString(), features.get(0).toString());
+        assertEquals(featureEntity.getName(), features.get(0).getName());
+        assertEquals(featureEntity.getNumberOfFeatures(),features.get(0).getNumberOfFeatures());
+
+        assertEquals(featureEntity1.toString(), features.get(1).toString());
+        assertEquals(featureEntity1.getName(), features.get(1).getName());
+        assertEquals(featureEntity1.getNumberOfFeatures(),features.get(1).getNumberOfFeatures());
+
+        filemanager.deleteFile(filemanager.getWorkDirectory() + path.getOutput() + "/DokerImagesFeatures[prova].csv");
+
     }
 }
