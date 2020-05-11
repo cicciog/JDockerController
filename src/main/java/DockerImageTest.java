09d7745147679d679df4359f0a49af7350b9ed35
cicciog <f.gualtieri@live.it>
2020-05-11 17:39:55 +0200

    updated csv files

--- src/main/java/DockerImageTest.java
+++ src/main/java/DockerImageTest.java
@@ -14,31 +14,16 @@ public class DockerImageTest {
     public static void main(String[] args) throws IOException, InterruptedException {
         DockerController dockercontroller = new DockerController();
 
-        //mageia
-        for (int i = 0; i < 2; i++) {
+        //Jetty
+        for (int i = 0; i < 10; i++) {
             dockercontroller.removeAllImages();
             int start1 = dockercontroller.getStartTime();
-            dockercontroller.buildDockerImage("mageia","/home/francesco/Documenti/repositories/juanluisbaptiste_docker-brew-mageia/build2release/.");
+            dockercontroller.buildDockerImage("mongo","/home/francesco/Documenti/repositories/docker-library_mongo/3.6/.");
             int end1 = dockercontroller.getFinalTime();
-            System.out.println("Esecuzione mageia " + (i + 1) + " " + (end1 - start1) + " secondi\n\n");
+            System.out.println("Esecuzione mongo" + (i + 1) + " " + (end1 - start1) + " secondi\n\n");
         }
         
-        //httpd
-        /* for (int i = 0; i < 9; i++) {
-            dockercontroller.removeAllImages();
-            int start1 = dockercontroller.getStartTime();
-            dockercontroller.buildDockerImage("httpd","/home/francesco/Documenti/repositories/docker-library_httpd/2.4/.");
-            int end1 = dockercontroller.getFinalTime();
-            System.out.println("Esecuzione httpd " + (i + 1) + " " + (end1 - start1) + " secondi\n\n");
-        }*/
- /* //busybox
-        for (int i = 0; i < 9; i++) {
-            dockercontroller.removeAllImages();
-            int start1 = dockercontroller.getStartTime();
-            dockercontroller.buildDockerImage("busybox","/home/francesco/Documenti/repositories/busybox/musl/.");
-            int end1 = dockercontroller.getFinalTime();
-            System.out.println("Esecuzione busybox " + (i + 1) + " " + (end1 - start1) + " secondi\n\n");
-        }*/
+      
     }
 
 }
