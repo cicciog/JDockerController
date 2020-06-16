c0a8bfd7d85e0d88888d51cdc0cfde2cf6f9af89
cicciog <f.gualtieri@live.it>
2020-06-16 18:51:12 +0200

    Jdocker controller first release

--- src/main/java/DockerImageTest.java
+++ src/main/java/DockerImageTest.java
@@ -14,24 +14,17 @@ public class DockerImageTest {
     public static void main(String[] args) throws IOException, InterruptedException {
         DockerController dockercontroller = new DockerController();
 
-        //haskell
+        //buildpack
         for (int i = 0; i < 10; i++) {
             dockercontroller.removeAllImages();
             int start1 = dockercontroller.getStartTime();
-            dockercontroller.buildDockerImage("haskell","/home/francesco/Documenti/repositories/haskell_docker-haskell/7.8");
+            dockercontroller.buildDockerImage("buidpack","/home/francesco/Documenti/repositories/docker-library_buildpack-deps/bionic");
             int end1 = dockercontroller.getFinalTime();
-            System.out.println("Esecuzione haskell" + (i + 1) + " " + (end1 - start1) + " secondi\n\n");
+            System.out.println("Esecuzione buildpack" + (i + 1) + " " + (end1 - start1) + " secondi\n\n");
         }
         
         
-       //bash devel
-        for (int i = 0; i < 10; i++) {
-            dockercontroller.removeAllImages();
-            int start1 = dockercontroller.getStartTime();
-            dockercontroller.buildDockerImage("bash","/home/francesco/Documenti/repositories/tianon_docker-bash/devel");
-            int end1 = dockercontroller.getFinalTime();
-            System.out.println("Esecuzione bash " + (i + 1) + " " + (end1 - start1) + " secondi\n\n");
-        }         
+             
       
     }
 
